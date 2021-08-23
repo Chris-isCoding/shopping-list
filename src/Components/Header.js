e4522d6a3f7151bc1dd6c6a73114a95f90b20314
Chris K <Chris-SourceCode@outlook.com>
2021-08-23 21:50:47 +0100

    feat: Pass title to Header component
      - implement default props

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,13 @@
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Shopping List</h1>
+      <h1>{title}</h1>
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: 'Groceries List',
 };
 
 export default Header;
