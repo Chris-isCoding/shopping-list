b718d687dd61ce5e519606b5f842e1db6a5a8827
Chris K <Chris-SourceCode@outlook.com>
2021-08-23 21:49:03 +0100

    feat: Move state to parent component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
+import { useState } from 'react';
 import Header from './Components/Header';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: 'Item 1',
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Item 2',
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Item 3',
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+  };
   return (
     <div className='App'>
-      <Header />
-      <Content />
-      <Footer />
+      <Header title={'Shopping List'} />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
