012f5ca41da76ba7766a3b53955d41ff59abf933
Chris K <Chris-SourceCode@outlook.com>
2021-08-23 20:05:16 +0100

    feat: Add default shopping list

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+
 const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: 'Item 1',
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Item 2',
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Item 3',
+    },
+  ]);
+
   return (
     <main>
-      <h2>Hello World</h2>
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input type='checkbox' checked={item.checked} />
+            <label>{item.item}</label>
+            <FaTrashAlt role='button' tabIndex='0' />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
